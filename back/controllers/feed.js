3be43a2bbea2ac081f7761594283a34078662d8b
unknown <maildeal@gmail.com>
2019-08-17 15:18:35 +0300

    332:add: validation for createPost data on server side

--- back/controllers/feed.js
+++ back/controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [{
@@ -14,6 +16,10 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
+  const validationErrors = validationResult(req);
+  if(!validationErrors.isEmpty()) {
+    return res.status(422).json({message: 'Validation failed', errors: validationErrors.array()})
+  }
   // Create post in db
   res.status(201).json({
     message: 'Post created successfully!',
