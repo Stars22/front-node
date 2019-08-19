86d77867d690b391521c888d7a36b3c3fe9a0416
Stars22 <maildeal@gmail.com>
2019-08-19 14:11:58 +0300

    336:add: fetch and render single post logic

--- back/controllers/feed.js
+++ back/controllers/feed.js
@@ -2,16 +2,38 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [{
-      _id: 1,
-      title: 'First Post',
-      content: 'This is the first post!',
-      imageUrl: 'images/image.jpg',
-      creator: { name: 'Ace'},
-      createdAt: new Date()
-     }]
-  });
+  Post.find()
+    .then(posts => {
+      res.status(200).json({
+        message: 'Fetched posts successfully',
+        posts
+      })
+    })
+    .catch(err => {
+      if(!err.statusCode) {
+        err.statusCode = 500;
+        next(err)
+      }
+    })
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(post => {
+      if(!post) {
+        const error = new Error('Could not find post');
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ message: 'Post fetched', post });
+    })
+    .catch(err => {
+      if(!err.statusCode) {
+        err.statusCode = 500;
+        next(err)
+      }
+    })
 };
 
 exports.createPost = (req, res, next) => {
