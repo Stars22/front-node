37c88750ea40a52057dbdf8db4cbb5c2cdec217a
Stars22 <maildeal@gmail.com>
2019-08-19 11:14:53 +0300

    334:add: logic to store post in db

--- back/controllers/feed.js
+++ back/controllers/feed.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
@@ -21,14 +22,18 @@ exports.createPost = (req, res, next) => {
     return res.status(422).json({message: 'Validation failed', errors: validationErrors.array()})
   }
   // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: { 
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: { name: 'Ace'},
-      createdAt: new Date()
-     }
+  const post = new Post({
+    title,
+    content,
+    imageUrl: '/someimage.jpg',
+    creator: { name: 'Ace'}
   });
+  post.save()
+    .then(result => {
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: result
+      });
+    })
+    .catch(err => next(new Error(err)));
 };
