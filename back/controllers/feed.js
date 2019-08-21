05cc935ec3c81d869bc5df715440a335eb67571e
Stars22 <maildeal@gmail.com>
2019-08-21 10:57:18 +0300

    338:add: logic for updating post

--- back/controllers/feed.js
+++ back/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 const Post = require('../models/post');
+const fs = require('fs');
+const path = require('path');
 
 exports.getPosts = (req, res, next) => {
   Post.find()
@@ -72,3 +74,53 @@ exports.createPost = (req, res, next) => {
       next(err);
     });
 };
+
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+  const validationErrors = validationResult(req);
+  if(!validationErrors.isEmpty()) {
+    const error = new Error('Validation failed, please enter valid data');
+    error.statusCode = 422;
+    return next(error);
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  let imageUrl = req.body.image;
+  if(req.file) {
+    imageUrl = req.file.path.includes('\\') ? req.file.path.replace(/\\/g,"/") : req.file.path;
+  }
+  if(!imageUrl) {
+    const error = new Error('No image picked');
+    error.statusCode = 422;
+    return next(error);
+  }
+  Post.findById(postId)
+    .then(post => {
+      if(!post) {
+        const error = new Error('Could not find post');
+        error.statusCode = 404;
+        throw error;
+      }
+      if(imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.content = content;
+      post.imageUrl = imageUrl;
+      return post.save()
+    })
+    .then(result => {
+      res.status(200).json({ message: 'Post updated', post: result });
+    })
+    .catch(err => {
+      if(!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+function clearImage(filePath) {
+  const picturePath = path.join(__dirname, '..', filePath);
+  fs.unlink(picturePath, err => console.log(err));
+}
