e8ef4ecb80cee66281e119df8fe7d7defe9099fe
Stars22 <maildeal@gmail.com>
2019-08-29 18:53:48 +0300

    374:add: mutation resolver for creating user

--- back/graphql/resolvers.js
+++ back/graphql/resolvers.js
@@ -1,8 +1,19 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
 module.exports = {
-    hello() {
-        return {
-            text: 'Hello world',
-            views: 1234
+    //destucture nested objects to pull password, email and name from {args}
+    async createUser({ userInput: {password, email, name } }, req){
+        const existingUser = await User.findOne({ email });
+        if(existingUser) {
+            const error = new Error('User exists already!')
+            throw error;
         }
+        const hashedPass = await bcrypt.hash(password, 12)
+        const user = new User({
+            email, name, password: hashedPass
+        });
+        const createdUser = await user.save();
+        return { ...createdUser._doc, _id: createdUser._id.toString() };
     }
 }
